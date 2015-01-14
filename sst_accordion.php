2a88adcb25be7b9884ae48b9adfa7ac2e438ad10
Craig Fox <craig@therichlandgroup.com>
2015-01-14 12:50:01 -0600

    setting up repo

---
+++ sst_accordion.php
@@ -0,0 +1,13 @@
+<?php
+   /*
+   Plugin Name: Simple Starter Accordion
+   Plugin URI: http://craigwfox.com
+   Description: Just a simple jquery accordion.
+   Version: 1.0
+   Author: Craig Fox
+   Author URI: http://craigwfox.com
+   License: GPL2
+   */
+
+   
+?>
